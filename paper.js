178bd558936bcb2c7fbcbd609d380db8200a3a51
Tajmansingh123 <76952196+Tajmansingh123@users.noreply.github.com>
2021-04-22 14:57:19 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class Paper{
+	constructor(x,y,r){
+    var options={
+		isStatic:true,
+		friction:0,
+		restitution:1,
+		density:1.2
+	}
+
+	this.x=x;
+	this.y=y;
+	this.r=r;
+	this.image=loadImage("paper.png");
+	this.body=Bodies.circle(this.x,this.y,this.r,options);
+	
+	World.add(world,this.body);
+	}
+ display(){
+	 var pos = this.body.position;
+	 push();
+	 translate(pos.x,pos.y);
+	 rectMode(CENTER);
+	 imageMode(CENTER);
+	 image(this.image,0,0,this.r,this.r);
+	 pop();
+ }
+}
